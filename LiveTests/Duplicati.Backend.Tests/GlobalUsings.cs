06ff8657a79d672bc973fce95d2aa895da9ad1df
Marcelo Ceccon <marcelo@duplicati.com>
2024-10-29 15:20:39 +0100

    Adding Live backend tests
    These tests perform not unit tests but actual backup tests to backends (both cloud and self hosted containers).

---
+++ LiveTests/Duplicati.Backend.Tests/GlobalUsings.cs
@@ -0,0 +1,11 @@
+// Global using directives
+
+global using System.Security.Cryptography;
+global using System.Security.Cryptography.X509Certificates;
+global using System.Text;
+global using DotNet.Testcontainers.Builders;
+global using DotNet.Testcontainers.Configurations;
+global using DotNet.Testcontainers.Containers;
+global using Duplicati.Backend.Tests.Base;
+global using Duplicati.Backend.Tests.Utility;
+global using Microsoft.VisualStudio.TestTools.UnitTesting;
